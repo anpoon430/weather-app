eccdec5b6c08dca57e1d94284c629d17f3fdd23e
Andy Poon <anpoon430@gmail.com>
2018-07-21 16:52:42 -0400

    shows basic weatherinfo

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 let loc = {};
 let url = 'https://fcc-weather-api.glitch.me/api/current?';
 
@@ -22,14 +23,49 @@ function getWeather(pos) {
 
 function displayWeather(data){
   let city = document.querySelector('#city');
-  let today = document.querySelector('#today');
+  let upper = document.querySelector('#upper');
+  let app = document.querySelector('#app');
   let iconURL = data.weather[0].icon;
-  city.textContent = data.name;
+  let header = document.querySelector('header');
+  let img = document.createElement('img');
+  let h2 = document.createElement('h2');
+  let h3 = document.createElement('h3');
+  let section = document.createElement('section');
+  let div = document.createElement('div');
+  let div2 = div.cloneNode();
+  let div3 = div.cloneNode();
   console.log(data);
-  let iconElement = document.createElement('img');
+  city.textContent = data.name;
+  h2.textContent = data.weather[0].description;
+  img.setAttribute('src', iconURL);
 
-  iconElement.setAttribute('src', iconURL);
-  today.appendChild(iconElement);
+  h2.setAttribute('id', 'weatherDescription');
+  setAttributes(section, {id:'lower', class:'row'});
+  setAttributes(div, {id:'humidity', class: 'col s4'});
+  setAttributes(div2, {id:'hitemp', class: 'col s4'});
+  setAttributes(div3, {id:'lotemp', class: 'col s4'});
+  h3.setAttribute('id', 'temp');
+  header.appendChild(h2);
+  upper.appendChild(img);
+  upper.appendChild(h3);
+  app.appendChild(section);
 
+  let lower = document.querySelector('#lower');
+  lower.appendChild(div);
+  lower.appendChild(div2);
+  lower.appendChild(div3);
+  let temp = document.querySelector('#temp');
+  temp.textContent = data.main.temp;
 
+  let hitemp = document.querySelector('#hitemp');
+  let lotemp = document.querySelector('#lotemp');
+  let humidity = document.querySelector('#humidity');
+  humidity.textContent = data.main.humidity;
+  hitemp.textContent = data.main['temp_max'];
+  lotemp.textContent = data.main['temp_min'];
+
+}
+
+function setAttributes(el, obj){
+  Object.keys(obj).forEach(attr => el.setAttribute(attr, obj[attr]));
 }
