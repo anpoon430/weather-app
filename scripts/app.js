7b2e2aa60f39d12f20f512fa154182019d7f8ff8
Andy Poon <anpoon430@gmail.com>
2018-07-21 15:05:20 -0400

    updated to use FCC api instead

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,27 @@
 let loc = {};
-let url =
-  "https://api.darksky.net/forecast/307bd1127a9a2339c5390298d120fa50/";
+let url = 'https://fcc-weather-api.glitch.me/api/current?';
 
 document.addEventListener("DOMContentLoaded", function() {
   navigator.geolocation.getCurrentPosition(getWeather, err);
 });
+function err(error) {
+  console.warn(`Error ${error.code}: ${error.message}`);
+}
 
 function getWeather(pos) {
   loc.lat = pos.coords.latitude;
   loc.long = pos.coords.longitude;
-  url += loc.lat + ',' + loc.long;
-  fetch(url, {mode: 'cors'})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(jsonData) {
-      console.log(jsonData);
-    })
-    .catch(function(error) {
+  url += `lat=${loc.lat}&lon=${loc.long}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(displayWeather)
+    .catch(error => {
       console.log('Oops! Something went wrong!', error);
     });
 }
-function err(error) {
-  console.warn(`Error ${error.code}: ${error.message}`);
+
+function displayWeather(data){
+  let city = document.querySelector('#city');
+  city.textContent = data.name;
+
 }
